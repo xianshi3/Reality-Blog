bc4747748126cc2b5b2e72663bd6c26949f635ad
xianshi3 <vm1784257751@foxmail.com>
2025-07-11 02:30:01 +0800

    Squashed commit of the following:
    commit 5cabd707d97447304244771a33de2cefd2df7593
    Merge: 99b98c0 939fafb
    Author: Reality <57677273+xianshi3@users.noreply.github.com>
    Date:   Fri Jul 11 02:29:18 2025 +0800
    
        Merge pull request #12 from xianshi3/admin-dev
    
        Admin dev
    
    commit 939fafb27b845108862096bd2604f8738aedfd4b
    Author: xianshi3 <vm1784257751@foxmail.com>
    Date:   Fri Jul 11 02:21:39 2025 +0800
    
        1.修复导航条重新位置错误问题 2.修复动画过渡导致打开网站出现白色问题
    
    commit a82f3eada34ea5e9abd42bec9f7cfce4e622f8e0
    Author: xianshi3 <vm1784257751@foxmail.com>
    Date:   Fri Jul 11 00:26:19 2025 +0800
    
        首页背景图层实现鼠标视差

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 /**
  * Header 头部组件
- * 
+ *
  * @module Header
  */
 
@@ -13,21 +13,21 @@ import ParallaxSection from "./ParallaxSection";
  */
 export default function Header() {
   return (
-    <header className="home-header">
-      {/* 固定导航条 */}
-      <Navbar />
+    <>
+      {/* 顶部导航条容器 */}
+      <div className="navbar-container">
+        <Navbar />
+      </div>
+
       {/* 视差背景区，居中显示，带圆角和动画 */}
       <div
         style={{ width: "100%", maxWidth: "1200px", margin: "0 auto" }}
         className="overflow-hidden rounded-3xl shadow-xl animate-fadeInDown transition-transform duration-700 ease-in-out hover:scale-105"
       >
-        <ParallaxSection
-          backgroundImage="/parallax-bg.jpg"
-          height={400}
-        >
+        <ParallaxSection backgroundImage="/parallax-bg.jpg" height={400}>
           {/* 可在此处填充内容 */}
         </ParallaxSection>
       </div>
-    </header>
+    </>
   );
 }
